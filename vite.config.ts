711f49f95ec6b3e49b2673b65650e883ef6251fe
acoulibaly-ghb <abdoulaye.coulibaly@gmail.com>
2025-12-11 01:56:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    'process.env.TEACHER_PASSWORD': JSON.stringify(process.env.TEACHER_PASSWORD),
+    // Cette ligne dit à Vite : "Si tu trouves 'process.env.API_KEY' dans le code, remplace-le par la valeur de VITE_API_KEY"
+    'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY || process.env.API_KEY),
+    
+    // Pareil pour le mot de passe professeur
+    'process.env.TEACHER_PASSWORD': JSON.stringify(process.env.VITE_TEACHER_PASSWORD || process.env.TEACHER_PASSWORD),
   },
 });
